fb6031d213e01177a0d80101404a383384354228
sourovmodak06 <sourovmodak650@gmail.com>
2023-03-30 21:26:21 +0600

    navigation bar done

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div className="px-10 md:px-20 ">
+      <Header></Header>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import user from "../images/user.png";
+
+const Header = () => {
+  return (
+    <div className="sticky top-0">
+      <div className="flex justify-between items-center my-5">
+        <h2 className="text-4xl font-bold">Knowledge Cafe</h2>
+        <img src={user} alt="User-images" className="rounded-full" />
+      </div>
+      <div className="h-[1px] bg-[#11111126]"></div>
+    </div>
+  );
+};
+
+export default Header;
